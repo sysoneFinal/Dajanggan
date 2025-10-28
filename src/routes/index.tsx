0d7693825afabfebe08f2b0e2818a06e13bc8026
dongcarry96 <fhakwpak6@naver.com>
2025-10-28 11:23:00 +0900

    refactor: Disk 경로 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,6 +23,7 @@ import BGWriterPage from "../pages/engine/BGWriterPage";
 import HotTablePage from "../pages/engine/HotTablePage";
 import HotIndexPage from "../pages/engine/HotIndexPage.tsx";
 import MemoryPage from "../pages/system/MemoryPage.tsx";
+import DiskPage from "../pages/system/DiskPage.tsx";
 
 const routeList = [
   // 헤더 없는 초기 페이지
@@ -36,7 +37,7 @@ const routeList = [
   // Instance 단위
   { path: "/instance/event", element: <EventPage /> },
   { path: "/instance/cpu/dashboard", element: <CpuPage /> },
-  // { path: "/instance/disk/dashboard", element: <DiskPage /> },
+  { path: "/instance/disk/dashboard", element: <DiskPage /> },
   { path: "/instance/memory/dashboard", element: <MemoryPage /> },
   { path: "/instance/checkpoint/dashboard", element: <CheckpointPage /> },
   { path: "/instance/bg-writer/dashboard", element: <BGWriterPage /> },
